8020cf1d749b3e02fcd5266a9441c486eacc4bd8
Panchanok Kaewchinda <kpanchanok@myseneca.ca>
2023-02-01 17:20:34 -0500

    edit file

--- blog-service.js
+++ blog-service.js
@@ -2,8 +2,6 @@ const fs = require("fs");
 var posts = [];
 var categories =[];
 
-//module.exports.readMessage = function () 
-
 module.exports.initialize = function () {
     return new Promise ((resolve, reject) => {
         fs.readFile('./data/posts.json', 'utf8', (err, data) => {
