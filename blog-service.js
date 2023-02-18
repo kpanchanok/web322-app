25739c4f927e7c58285ed89e8c02bf5f3ee1e83f
Panchanok Kaewchinda <kpanchanok@myseneca.ca>
2023-02-18 16:20:46 -0500

    add assignment 3

--- blog-service.js
+++ blog-service.js
@@ -1,9 +1,10 @@
 const fs = require("fs");
+const { resolve } = require("path");
 var posts = [];
 var categories =[];
 
 module.exports.initialize = function () {
-    return new Promise ((resolve, reject) => {
+    return new Promise ((resolve, reject) => { //object (so it can return)
         fs.readFile('./data/posts.json', 'utf8', (err, data) => {
             if (err) {
                 reject ("unable to read file");
@@ -52,3 +53,64 @@ module.exports.getCategories = function () {
         resolve(categories);
     })
 };
+
+module.exports.addPost = function (postData){
+    return new Promise ((resolve, reject) => {
+
+        //this gets around the issue of the checkbox not sending "false" if it's unchecked)
+        if(postData.published === undefined){
+            postData.published = false;
+        }
+        else{
+            postData.published = true;
+        }
+
+        //This will have the effect of setting the first new post's id to: 31, and so on.
+        postData.id = posts.lenght + 1
+
+        posts.push(postData);
+        resolve(postData);
+    })
+}
+
+//not finish
+module.exports.getPostsByCategory = function (category){
+    return new Promise ((resolve, reject) => {
+        let filteredPosts = posts.filter(post=>post.category == category);
+
+        if(filteredPosts.length == 0){
+            reject("no results returned")
+        }else{
+            resolve(filteredPosts);
+        }
+        resolve(categories);
+    })
+}
+
+//not finish
+module.exports.getPostsbyMinDate = function (minDateStr){
+    return new Promise((resolve,reject) => {
+        let filteredDate = posts.filter(post=> (new Date(post.postDate)) >= (new Date(minDateStr)));
+
+        if(filteredDate.length == 0){
+            reject("no results returned")
+        }else{
+            resolve(filteredDate);
+        }
+    })
+}
+
+//unique ID that match value
+module.exports.getPostById = function (id){
+    return new Promise((resolve, reject) => {
+        const filteredID = posts.filter(post => post.id == id);
+        const uniquePost = filteredID[0];
+
+        if (uniquePost) {
+            resolve(uniquePost);
+        }
+        else {
+            reject("No results returned");
+        }
+    })
+}
--- server.js
+++ server.js
@@ -1,9 +1,10 @@
 /*********************************************************************************
-*  WEB322 – Assignment 02
-*  I declare that this assignment is my own work in accordance with Seneca  Academic Policy.  No part *  of this assignment has been copied manually or electronically from any other source 
+*  WEB322 – Assignment 03
+*  I declare that this assignment is my own work in accordance with Seneca  Academic Policy.  No part 
+*  of this assignment has been copied manually or electronically from any other source 
 *  (including 3rd party web sites) or distributed to other students.
 * 
-*  Name: Panchanok Kaewchinda Student ID: 145443214 Date: 2/1/2023
+*  Name: Panchanok Kaewchinda Student ID: 145443214 Date: 18/2/2023
 *
 *  Online (Cyclic) Link: https://bright-scarf-dove.cyclic.app
 *
@@ -18,6 +19,20 @@ var HTTP_PORT = process.env.PORT || 8080;
 var path = require("path");
 var blogjs = require(__dirname + "/blog-service.js");
 
+const multer = require("multer");
+const cloudinary = require('cloudinary').v2
+const streamifier = require('streamifier');
+const { getPostById, addpost, addPost, getPostsByCategory } = require("./blog-service");
+const { title } = require("process");
+const upload = multer(); // no { storage: storage } since we are not using disk storage
+
+//Set the cloudinary config
+cloudinary.config({
+    cloud_name: "dklclh0wa",
+    api_key: "575892647679222",
+    api_secret: "BMrciX3lTYISVFOHmyMiKXkfJBg",
+    secure: true
+  });
 
 function onHttpStart() {
   console.log("Express http server listening on: " + HTTP_PORT);
@@ -42,7 +57,24 @@ app.get("/blog", function(req,res){
 });
 
 app.get("/posts", function(req,res){
-    blogjs.getPublishedPosts().then((data) => {
+    /*blogjs.getPublishedPosts().then((data) => {
+        res.json({data});
+    }).catch((err) => {
+        res.json({message: err});
+    })*/
+
+    let data = null;
+    if (req.query.category){
+        data = blogData.getPostsByCategory(req.query.category);
+    }
+    else if(req.query.minDate){
+        data = blogData.getPostsbyMinDate(req.query.minDate);
+    }
+    else{
+        data = blogjs.getAllPosts();
+    }
+
+    data.then((data) => {
         res.json({data});
     }).catch((err) => {
         res.json({message: err});
@@ -56,6 +88,72 @@ app.get("/categories", function(req,res){
         res.json({message: err});
     })
 });
+
+app.get("/posts/add", function(req,res){
+    res.sendFile(path.join(__dirname,"/views/addPost.html"));
+});
+
+app.post('/posts/add',upload.single("featureImage"), function (req, res) {
+    let streamUpload = (req) => {
+        return new Promise((resolve, reject) => {
+            let stream = cloudinary.uploader.upload_stream(
+                (error, result) => {
+                if (result) {
+                    resolve(result);
+                } else {
+                    reject(error);
+                }
+                }
+            );
+    
+            streamifier.createReadStream(req.file.buffer).pipe(stream);
+        });
+    };
+    
+    async function upload(req) {
+        let result = await streamUpload(req);
+        console.log(result);
+        return result;
+    }
+    
+    upload(req).then((uploaded)=>{
+        req.body.featureImage = uploaded.url;
+        
+        // TODO: Process the req.body and add it as a new Blog Post before redirecting to /posts
+        
+        blogData.addPost(req.body).then(post=>{
+            res.redirect("/posts");
+        }).catch(err=>{
+            res.status(500).send(err);
+        })
+        //res.redirect("/posts");
+
+    }).catch((err) => {
+        res.send(err);
+      });
+    
+});
+
+//part3
+//posts?category=value 
+app.get("/post?category:value", (req,res) => {
+    getPostsByCategory(req.params.value).the((data) => {
+        res.send(data)
+    }).catch((err) => {
+        res.send(err);
+    })
+});
+
+//check id that match value
+app.get("/post/:value", (req, res) => {
+    getPostById(req.params.value).then((data) => {
+        res.send(data)
+    }).catch((err) => {
+        res.send(err);
+      });
+});
+
+
 
 app.use((req, res) => {
     res.status(404).sendFile(path.join(__dirname,"/views/404page.html"));
